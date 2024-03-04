64ce9b40e30c0e8180c8d2af9f88115cd3da1dbb
jinyuttt <jinyu20072787@sina.com>
2024-03-04 23:08:08 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AnalysisSQL;
+
+Console.WriteLine("Hello, World!");
+string sql = "select * from student m join  Test n  on m.id=n.id";
+SqlParser parser = new SqlParser();
+parser.Parse(sql);
+Console.WriteLine(parser.ToText());
